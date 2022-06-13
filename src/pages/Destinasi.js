422ce2f1ef714c85720998782d18ff77e51142bf
Anrian Sihotang <anrisihotang91@gmail.com>
2022-06-13 22:52:20 +0700

    menambahkan commponent Header di destinasi.js

--- src/pages/Destinasi.js
+++ src/pages/Destinasi.js
@@ -2,16 +2,18 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import HeroDestinasi from '../components/HeroDestinasi'
 import KontenDestinasi from '../components/KontenDestinasi';
+import Header from '../components/Header';
 
 const Destinasi = () => {
     const {id} = useParams();
     return (
-        <div>
+        <>
+			<Header/>
             <HeroDestinasi destinasiID= {id}/>
             <section>
                 <KontenDestinasi destinasiID= {id}/>
             </section>
-        </div>
+        </>
     )
 }
 
