c4b7fa822ff61700f8db991fde85f49c83a43431
LuckyPice <106434753+LuckyPice@users.noreply.github.com>
2022-07-03 12:02:53 +0700

    Update UMKM.js

--- src/pages/UMKM.js
+++ src/pages/UMKM.js
@@ -1,17 +1,27 @@
 import React, { Component } from "react";
-import Header from "../components/Header";
-import HeroUMKM from "../components/HeroUMKM";
+import HeroDestination from "../components/HeroDestination";
 import MainUMKM from "../components/MainUMKM";
 import Recommendationsumkm from "../components/recommendationsumkm";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import  MainUMK from "../components/MainUMK";
 
 class UMKM extends Component {
 	render() {
 		return (
+			
 			<div>
 				<Header />
-				<HeroUMKM/>
+	
+				<HeroDestination />
+
+
 				<MainUMKM/>
+				< MainUMK/>
+				<section>
 				<Recommendationsumkm/>
+				</section>
+				<Footer />
 			</div>
 			
 		)
